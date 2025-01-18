2b2f21960f927b221f40200e3441ee718608984d
Simon Ivarsson <143069272+SimonIvarsson@users.noreply.github.com>
2025-01-18 15:53:04 +0100

    Första sidan
    Ger kort information till användaren (som inte stämmer) för att få användaren att känna sig trygg

---
+++ Start sidan/index.js
@@ -0,0 +1,6 @@
+const next_btn = document.getElementById("btn-next");
+next_btn.addEventListener("click", function(){
+    setTimeout(function () {
+        window.location.href = "../loginSidan.html";},
+        3000); 
+    });
